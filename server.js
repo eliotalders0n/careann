ef5ddfb987a2fc4686ddec05507b1d9f6569becf
eliotalders0n <eliot.alderson@gmail.com>
2023-06-05 18:05:38 +0200

    server change

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import express, { urlencoded } from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import cors from "cors";
-import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
 import mongoose from "mongoose";
@@ -50,17 +49,6 @@ app.get("*", (req, res) =>
 app.use((err, res, req, next) => {
   res.status(500).send({ message: err.message });
 });
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "public/assets");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
-
-const upload = multer({ storage });
 
 // Routes with files
 app.post("/auth/register", upload.single("picture"), register);
